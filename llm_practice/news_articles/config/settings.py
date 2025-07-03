8fd12200103c15e932db8f635b7c116c89599cef
thirupathirao999 <thirupathi.molamanti@thezennialpro.com>
2025-07-03 13:41:54 +0530

    adding image hadlling

--- llm_practice/news_articles/config/settings.py
+++ llm_practice/news_articles/config/settings.py
@@ -1,7 +1,7 @@
 BASE_URL = "https://newsapi.org/v2/everything"
 NEWS_API_KEY = "43a1035ead4349feaffa8324ca3f540b"
 DEFAULT_QUERY = "bitcoin"
-MAX_PAGES = 5
+MAX_PAGES = 1
 PAGE_SIZE = 5
 QUEUE_DIR = "queue"
 LOG_DIR = "logs"
--- llm_practice/news_articles/worker/fetch_news.py
+++ llm_practice/news_articles/worker/fetch_news.py
@@ -5,9 +5,15 @@ import json
 import uuid
 from config.settings import BASE_URL, NEWS_API_KEY, DEFAULT_QUERY,MAX_PAGES, PAGE_SIZE, QUEUE_DIR, LOG_DIR
 from utils.logger import get_logger
+from utils.image_handler import handle_article_image
 from datetime import datetime, timezone
+from tqdm import tqdm
 
 logger = get_logger('fetch_news')
+
+def ensure_directories():
+    os.makedirs(QUEUE_DIR,exist_ok=True)
+    os.makedirs(LOG_DIR,exist_ok=True)
 
 def fetch_articles(query):
     all_articles = []
@@ -18,26 +24,39 @@ def fetch_articles(query):
             "page": page, 
             "apiKey" : NEWS_API_KEY
         }
-        response = requests.get(BASE_URL, params= params)
-        logger.info (f"Fetching the Page {page} -> {response.status_code} -> {response.url} ")
-        response.raise_for_status()
-        data = response.json()
-        if "articles" in data:
-            all_articles.extend(data["articles"])
-            logger.info (f"Page {page} : Retrived {len(data["articles"])} articles.")
-        else:
-            logger.info (f"Page {page} : Dose not have articles ")
+        try:
+            response = requests.get(BASE_URL, params= params)
+            logger.info (f"Fetching the Page {page} -> {response.status_code} -> {response.url} ")
+            response.raise_for_status()
+            data = response.json()
+            if "articles" in data:
+                all_articles.extend(data["articles"])
+                logger.info (f"Page {page} : Retrived {len(data["articles"])} articles.")
+            else:
+                logger.info (f"Page {page} : Dose not have articles ")
+        except Exception as e:
+            logger.error(f"Error fetching page{page}: {e}")
     return all_articles
 
 
 def save_article (article):
     article_id = str (uuid.uuid4())
     article["fetched_at"] = datetime.now(timezone.utc).isoformat()
-    file_path = os.path.join(QUEUE_DIR, f"{article_id}.json")
-    with open (file_path, "w", encoding="utf-8") as f:
-        json.dump(article, f, indent=2)
-        logger.info (f"Saved article to {file_path}")
-    return True
+
+    folder_path = os.path.join(QUEUE_DIR, article_id)
+    os.makedirs(folder_path,exist_ok=True)
+
+    image_path=handle_article_image(article.get("urlToImage"),folder_path,article_id)
+    article["article_image_original"] = image_path.replace("\\", "/")
+
+    json_path = os.path.join(folder_path, f"{article_id}.json")
+    try:
+        with open(json_path, "w", encoding="utf-8") as f:
+            json.dump(article, f, ensure_ascii=False, indent=2)
+        logger.info(f"Saved article to {json_path}")
+    except Exception as e:
+        logger.error(f"Error saving article {article_id}: {e}")
+
 
 def main():
     logger.info ("Starting fetch for api....")
@@ -47,7 +66,7 @@ def main():
 
     # Save Articles in Folder
 
-    for idx, article in enumerate (articles):
+    for article in tqdm(articles):
         if article.get("title") and article.get("url"):
             save_article(article)
 
